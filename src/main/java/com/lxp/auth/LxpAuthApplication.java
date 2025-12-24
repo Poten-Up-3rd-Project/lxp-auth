55abb0d749624683fd131f5cb19a073ef8d369cf
dnya0 <annay01794@gmail.com>
2025-12-24 15:15:28 +0900

    chore: initialize project

---
+++ src/main/java/com/lxp/auth/LxpAuthApplication.java
@@ -0,0 +1,13 @@
+package com.lxp.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxpAuthApplication.class, args);
+    }
+
+}
